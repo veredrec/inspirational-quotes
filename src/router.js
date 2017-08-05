6c5ecef5334e2671f0487cad70db5ec293b6a3ee
Vered Rekanati <veredrec@gmail.com>
2017-08-05 11:00:09 +0300

    add centent to server and create files functions in router and handler Relates #2

--- src/router.js
+++ src/router.js
@@ -1,4 +1,3 @@
-
 const handler = require('./handler.js');
 
 const router = (request, response) => {
@@ -7,8 +6,6 @@ const router = (request, response) => {
     handler.handleHome(response);
   } else if (url.indexOf('/public/') === 0) {
     handler.handlePublic(response, url);
-  // } else if (url.indexOf('/coins') === 0) {
-  //   handler.handleInput(request, response);
   } else {
     response.writeHead(404, {'Content-Type': 'text/html'});
     response.end('<h1>File not found</h1>');
